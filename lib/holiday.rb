106464628eeff6b55945059f384c284619bee02c
heyserenadinh Dinh <heyserenadinh@gmail.com>
2020-04-05 04:10:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -44,6 +44,9 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
+  holiday_hash.each do |season, holiday_with_supplies|
+  puts "#{season.capitalize}:"
+  holiday_with_supplies.each do 
 
 end
 
